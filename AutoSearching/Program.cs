13792dbed918e4e4ead6f702b3e1c2f0381f4701
Patrick <thin_spirits@hotmail.com>
2023-12-14 18:23:44 -0500

    Browser is able to open to the site requested

--- AutoSearching/Program.cs
+++ AutoSearching/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 
 namespace AutoSearching
 {
@@ -7,7 +6,34 @@ namespace AutoSearching
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                //TODO: set number of searches
+                //TODO: obtain different search targets
+                //TODO: set group number
+                //TODO: for each group, run number of searches
+                //TODO: set wait time between searches
+                //TODO: set wait time during group
+                
+                
+                //set site
+                var target = "http://www.bing.com";
+                
+                //sets process start to shell and sets site from target
+                var psi = new ProcessStartInfo();
+                psi.UseShellExecute = true;
+
+                psi.FileName = target;
+
+                //opens browser
+                Process.Start(psi);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+
         }
+        
     }
 }
