95345e5ae37a7c6ceb542965f2ef20907c4b67fd
Patrick <thin_spirits@hotmail.com>
2023-12-15 15:12:03 -0500

    working out the process

--- AutoSearching/Program.cs
+++ AutoSearching/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Collections;
 
 namespace AutoSearching
 {
@@ -8,31 +10,88 @@ namespace AutoSearching
         {
             try
             {
-                //TODO: set number of searches
-                //TODO: obtain different search targets
-                //TODO: set group number
-                //TODO: for each group, run number of searches
-                //TODO: set wait time between searches
-                //TODO: set wait time during group
-                
-                
-                //set site
-                var target = "http://www.bing.com";
-                
-                //sets process start to shell and sets site from target
+
                 var psi = new ProcessStartInfo();
                 psi.UseShellExecute = true;
 
-                psi.FileName = target;
+                var totalSearchesAllowed = (150/5); //30 change later to gather from writeline as some searches may have been completed
+                var searchesAllowedPerGroup =  3;   //all change as this may change in the future
+                var groupAmount = (totalSearchesAllowed/searchesAllowedPerGroup); //10
+                var currentGroup = 0;
 
-                //opens browser
-                Process.Start(psi);
+                var waitTime = GetWaitTime();
+                var groupWaitTime = GetWaitTime("group");
+
+                var allSearchTerms = GetTodaysSearches(totalSearchesAllowed);
+
+
+                while (currentGroup > groupAmount)
+                {
+                    
+                    //TODO: assign currentSearchTerm to next search term in index
+
+                    var currentSearchTerm = new List<string>();
+                    
+                    for (int currentSearchesInGroup = 0; currentSearchesInGroup < searchesAllowedPerGroup; currentSearchesInGroup++)
+                    {
+                        //TODO: open a browser to next search term in current group
+                        psi.FileName = currentSearchTerm[currentSearchesInGroup];
+
+                        //sets process start to shell and sets site from target
+                    
+
+                        //opens browser
+                        Process.Start(psi);
+                        currentSearchesInGroup++;
+
+                        //TODO: set next search term
+
+                        Thread.Sleep(groupWaitTime);
+
+
+                    }
+                    currentGroup++;
+                    
+                    Thread.Sleep(waitTime);
+
+                    //TODO: repeat until all groups have been searched
+
+                }
+
             }
             catch (Exception exception)
             {
                 Console.WriteLine(exception);
             }
 
+        }
+
+        private static List<string> GetTodaysSearches(int totalSearchesAllowed)
+        {
+            
+            //this is only a test to return something
+            return new List<string> { "one", "two", "three" };
+
+
+            //TODO: use total search number to fill list with different searches according to total search number
+
+            /*
+            These were used initially, may remove once searches are filled
+              List.Add("http://www.bing.com");
+              return searchTerms;
+            */
+        }
+
+
+        static int GetWaitTime(string? waitParameter=null)
+        {
+            var waitTime = 0;
+            if (waitParameter == "group")
+                waitTime = 8000;//may need to gather from user
+
+            else waitTime = 15000;//may need to gather from user
+
+            return waitTime;
         }
         
     }
